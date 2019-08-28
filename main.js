d811b60458b3aabd44009c89d1fe31119cf7b011
estudiante <estudiante@SC310E13.local>
2019-08-27 19:08:39 -0500

    push breve, check animado en botón

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var btn = document.querySelector('button');
 var span = btn.querySelector('span');
 var loader = document.createElement('span');
 
+var checkIconPath = document.querySelector('.check-icon path');
+
 function btnClick () {
     if(btn.classList.contains('circle')){
         return;
@@ -17,6 +19,19 @@ function btnClick () {
     setTimeout(function() {
         loader.classList.add('fade-in');
     }, 500);
+
+    /*for(let i = 400; i < 800; i += 10){
+        checkIconPath.style.strokeDashoffset = i;
+    }*/
+
+    var count = 0;
+    var intervalId = setInterval(function() {
+        checkIconPath.style.strokeDashoffset = count + 400;
+        count += 10;
+        if(count >= 400){
+            clearInterval(intervalId);
+        }
+    }, 16);
 }
 
 btn.addEventListener('click', btnClick);
